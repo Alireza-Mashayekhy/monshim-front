c36cf7d9c2ebc464290ecc527e3380db66ac9266
AlirezaMashayekhy <alireza@sheydal.com>
2026-09-24 13:07:37 +0330

    fix(sentry): change test for verify sentry

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,16 @@
+'use client';
+import axios from 'axios';
+
 export default function AuthRootRedirect() {
+  const onClick = async () => {
+    await axios.get('https://monshiim.ir/api/debug-sentry');
+    throw new Error('Sentry Test Error');
+  };
   return (
-    <button
-      type="button"
-      onClick={() => {
-        throw new Error('Sentry Test Error');
-      }}
-    >
-      Break the world
-    </button>
+    <div>
+      <button type="button" onClick={onClick}>
+        Break the world
+      </button>
+    </div>
   );
 }
